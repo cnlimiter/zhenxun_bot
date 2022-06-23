23d0f383e7b4a4f1058bb591ce63c94a74f83396
cnlimiter <cnlimiter@163.com>
2022-06-23 20:36:40 +0800

    some fix

--- plugins/wws/utils.py
+++ plugins/wws/utils.py
@@ -1,10 +1,20 @@
 import gzip
 import io
+import os
 import time
 from collections import defaultdict
 from datetime import datetime, timedelta
 
+import nonebot
 import pytz
+from nonebot import Driver
+from nonebot.adapters.onebot.v11 import MessageSegment
+
+from plugins.sign_in.config import SIGN_TODAY_CARD_PATH
+from plugins.wws.config import WSS_PATH, WSS_SEARCH_PATH
+from utils.image_utils import BuildImage
+from utils.message_builder import image
+driver: Driver = nonebot.get_driver()
 
 
 async def match_keywords(match_list, Lists):
@@ -74,3 +84,46 @@ class DailyNumberLimiter:
 
     def reset(self, key):
         self.count[key] = 0
+
+
+@driver.on_startup
+async def init_image():
+    WSS_PATH.mkdir(parents=True, exist_ok=True)
+    WSS_SEARCH_PATH.mkdir(exist_ok=True, parents=True)
+    clear_wss_data_pic()
+
+
+def clear_wss_data_pic():
+    date = datetime.now().date()
+    for file in os.listdir(WSS_SEARCH_PATH):
+        if str(date) not in file:
+            os.remove(WSS_SEARCH_PATH / file)
+
+
+async def _gene_help_img(
+    text: str,
+    width: int,
+    height: int
+                  ) -> MessageSegment:
+    current_date = datetime.now()
+    data = current_date.date()
+    info_img = BuildImage(width, height, color=(255, 255, 255, 0), font_size=15)
+    info_img.text((0, 0), f"{text}")
+    info_img.save(WSS_SEARCH_PATH / f"{data}_help.png")
+    return image(f"{data}_help.png", "wss")
+
+
+def _gene_info_img(
+
+    group_id: int,
+    user_id: int,
+    text: str,
+    width: int,
+    height: int
+                  ) -> MessageSegment:
+    current_date = datetime.now()
+    data = current_date.date()
+    info_img = BuildImage(width, height, color=(255, 255, 255, 0), font_size=15)
+    info_img.text((0, 0), f"{text}")
+    info_img.save(SIGN_TODAY_CARD_PATH / f"{user_id}_{group_id}_{data}.png")
+    return image(f"{user_id}_{group_id}_{data}.png", "sign/today_card")
