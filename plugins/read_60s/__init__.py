d7070e3a05d42429b88ea7aa6cbec9dc58d08fe1
cnlimiter <cnlimiter@163.com>
2022-06-23 19:54:15 +0800

    fix wws

--- plugins/read_60s/__init__.py
+++ plugins/read_60s/__init__.py
@@ -25,30 +25,30 @@ __plugin_settings__ = {
     ],
 }
 
-
 global_config = nonebot.get_driver().config
 nonebot.logger.info("global_config:{}".format(global_config))
 plugin_config = Config(**global_config.dict())
 nonebot.logger.info("plugin_config:{}".format(plugin_config))
 scheduler = require("nonebot_plugin_apscheduler").scheduler  # type:AsyncIOScheduler
 
+
 def remove_upprintable_chars(s):
-    return ''.join(x for x in s if x.isprintable())#去除imageUrl可能存在的不可见字符
+    return ''.join(x for x in s if x.isprintable())  # 去除imageUrl可能存在的不可见字符
+
 
 async def read60s():
-    #global msg  # msg改成全局，方便在另一个函数中使用
+    # global msg  # msg改成全局，方便在另一个函数中使用
     msg = await suijitu()
     for qq in plugin_config.read_qq_friends:
         await nonebot.get_bot().send_private_msg(user_id=qq, message=Message(msg))
 
     for qq_group in plugin_config.read_qq_groups:
-        await nonebot.get_bot().send_group_msg(group_id=qq_group, message=Message(msg))# MessageEvent可以使用CQ发图片
-
+        await nonebot.get_bot().send_group_msg(group_id=qq_group, message=Message(msg))  # MessageEvent可以使用CQ发图片
 
 
 async def suijitu():
     try:
-        url="https://api.iyk0.com/60s"
+        url = "https://api.iyk0.com/60s"
         resp = requests.get(url)
         resp = resp.text
         resp = remove_upprintable_chars(resp)
@@ -57,7 +57,7 @@ async def suijitu():
         pic_ti = f"今日60S读世界已送达\n[CQ:image,file={lst}]"
         return pic_ti
     except:
-        url = "https://api.2xb.cn/zaob"#备用网址
+        url = "https://api.2xb.cn/zaob"  # 备用网址
         resp = requests.get(url)
         resp = resp.text
         resp = remove_upprintable_chars(resp)
@@ -65,6 +65,7 @@ async def suijitu():
         lst = retdata['imageUrl']
         pic_ti1 = f"今日60S读世界已送达\n[CQ:image,file={lst}]"
         return pic_ti1
+
 
 for index, time in enumerate(plugin_config.read_inform_time):
     nonebot.logger.info("id:{},time:{}".format(index, time))
