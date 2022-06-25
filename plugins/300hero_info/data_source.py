0c5ef31c70b5f438e67cd6fe4784c2a3006b2090
cnlimiter <cnlimiter@163.com>
2022-06-26 07:52:36 +0800

    update shop daily_limit

--- plugins/300hero_info/data_source.py
+++ plugins/300hero_info/data_source.py
@@ -1,11 +1,14 @@
 import traceback
+from pathlib import Path
 
+dir_path = Path(__file__).parent
+template_path = dir_path / "template"
 
 async def set_role_info_params(List):
     try:
 
         result = {
-
+            "template_path": template_path,
         }
         return result
     except Exception:
